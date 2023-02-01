c981c14d187fcc31f3201a3df6018bb9c554cbdb
Akash Ajith <akash.ajith3@gmail.com>
2023-02-02 03:45:22 +0530

    first commit

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key ='05b5237f872e38af8474490f3f1f8c45'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  export default requests
